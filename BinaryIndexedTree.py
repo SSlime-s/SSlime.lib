86f4da0159cc7ed26802e24dd5642779c65860ed
しゅーまい <62363188+Shumaislime@users.noreply.github.com>
2020-06-27 09:53:01 +0900

    BITfix
    idxを1-indexed1から0-indextedへ(内部での処理は同じ)

--- BinaryIndexedTree.py
+++ BinaryIndexedTree.py
@@ -2,14 +2,16 @@ class BinaryIndexedTree:
     def __init__(self,n,default = 0):
         self.s = [default]*(n+1)
         self.n = n
-    # idxは1-indexted
+    # idxは0-indexted
     def add(self,val,idx):
+        idx += 1
         while idx < self.n+1:
             self.s[idx] = self.s[idx] + val
             idx += idx&(-idx)
         return
     
     def get(self,idx):
+        isx += 1
         res = 0
         while idx > 0:
             res = res + self.s[idx]
