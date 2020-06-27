3f14e62959652266555ce48c135b458d553e98ba
しゅーまい <62363188+Shumaislime@users.noreply.github.com>
2020-06-27 17:39:28 +0900

    flakeを適応

--- BinaryIndexedTree.py
+++ BinaryIndexedTree.py
@@ -1,19 +1,23 @@
 class BinaryIndexedTree:
-    def __init__(self,n,default = 0):
+    def __init__(self, n, default=0):
+        "nの長さのBITをdefaultを初期値として構築する"
         self.s = [default]*(n+1)
         self.n = n
     # idxは0-indexted
-    def add(self,val,idx):
+
+    def add(self, idx, val):
+        "idx(0-indexted)の値にvalを加える"
         idx += 1
         while idx < self.n+1:
             self.s[idx] = self.s[idx] + val
-            idx += idx&(-idx)
+            idx += idx & (-idx)
         return
-    
-    def get(self,idx):
+
+    def get(self, idx):
+        "idx(0-indexted)以下の要素の和を求める"
         idx += 1
         res = 0
         while idx > 0:
             res = res + self.s[idx]
-            idx -= idx&(-idx)
+            idx -= idx & (-idx)
         return res
