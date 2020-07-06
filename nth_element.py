e2ae4f597cd58065837c52a0d76baa89afd58e57
しゅーまい <62363188+Shumaislime@users.noreply.github.com>
2020-07-06 15:18:11 +0900

    fix nth_element.py

--- nth_element.py
+++ nth_element.py
@@ -1,22 +1,23 @@
 def nth_element(array, will):
     "arrayの[0,will)を大きいほうからwill(1-indexted)番目までの要素にする"
+    will -= 1
     n = len(array)
     left = 0
     right = n-1
-    while True:
+    while right > left:
         i = left-1
         j = right
         v = array[right]
         while True:
             i += 1
             j -= 1
-            while array[i]<v:i+=1
-            while array[j]>v:j-=1
-            if i=>j:break
+            while array[i]>v:i+=1
+            while array[j]<v:j-=1
+            if i>=j:break
             array[i],array[j] = array[j],array[i]
-        array[i],array[r] = array[r],array[i]
-        if i == k:break
-        if i < k:
+        array[i],array[right] = array[right],array[i]
+        if i == will:break
+        if i > will:
             right = i-1
         else:
             left = i+1
